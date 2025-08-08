5fa8f901593d35571cf7f9d207626d074afa9765
Long <long06072003@gmai.com>
2025-08-08 17:16:41 +0700

    feat: component common

--- app/routes/dev/component.tsx
+++ app/routes/dev/component.tsx
@@ -1,4 +1,4 @@
-import { ConfigProvider, List } from "antd";
+import { List } from "antd";
 import React, { useState } from "react";
 import MyAvatar from "~/components/ui/avatar";
 import MyButton from "~/components/ui/button";
@@ -7,10 +7,59 @@ import MyDatePicker from "~/components/ui/date-picker";
 import MyInput from "~/components/ui/input";
 import MyInputNumber from "~/components/ui/input-number";
 import MyModal from "~/components/ui/modal";
+import MyRadio from "~/components/ui/radio";
+import MySelect from "~/components/ui/select";
 import MyTable from "~/components/ui/table";
 import MyTimePicker from "~/components/ui/time-picker";
 import UploadCP from "~/components/ui/upload";
+import { UserOutlined } from "@ant-design/icons";
 
+const data = [
+  "Racing car sprays burning fuel into crowd.",
+  "Japanese princess to wed commoner.",
+  "Australian walks 100km after outback crash.",
+  "Man charged over missing wedding girl.",
+  "Los Angeles battles huge wildfires.",
+];
+const dataSource = [
+  {
+    key: "1",
+    name: "Mike",
+    age: 32,
+    address: "10 Downing Street",
+  },
+  {
+    key: "2",
+    name: "John",
+    age: 42,
+    address: "10 Downing Street",
+  },
+];
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+    key: "age",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+    key: "address",
+  },
+];
+const onChange = (value: string) => {
+  console.log(`selected ${value}`);
+};
+
+const onSearch = (value: string) => {
+  console.log("search:", value);
+};
 const Dev: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -23,21 +72,18 @@ const Dev: React.FC = () => {
     setIsOpen(false);
   };
   return (
-    <ConfigProvider
-      theme={{
-        token: {
-          // colorText: "#fff",
-          colorPrimary: "#000",
-          borderRadius: 20,
-          // colorBgContainer: "#e30713",
-        },
-      }}
-    >
-      <MyButton style={{ padding: "20px 30px" }} label="Button" />
+    <div className="p-[100px]">
+      <MyAvatar size={64} icon={<UserOutlined />} />
+      <MyButton
+        style={{ padding: "20px 30px", margin: "0px 30px" }}
+        label="Button"
+        type="primary"
+      />
       <MyButton
         style={{ padding: "20px 30px" }}
         label="Mở pop-up"
         onClick={() => setIsOpen(true)}
+        type="primary"
       />
       <MyModal
         open={isOpen}
@@ -47,16 +93,54 @@ const Dev: React.FC = () => {
       >
         Bạn có chắc chắn muốn thực hiện hành động này không?
       </MyModal>
-      <MyInput />
-      <MyInputNumber />
+      <MyCheckBox style={{ margin: "0px 30px" }} />
+      <MyInput
+        style={{ padding: "5px 20px", width: "200px", margin: "0px 30px" }}
+      />
+      <MyInputNumber style={{ margin: "0px 30px" }} />
       <UploadCP />
       <MyDatePicker />
       <MyTimePicker />
-      <MyCheckBox />
-      <List />
-      <MyTable />
-      <MyAvatar />
-    </ConfigProvider>
+      <MySelect
+        showSearch
+        placeholder="Select a person"
+        optionFilterProp="label"
+        onChange={onChange}
+        onSearch={onSearch}
+        options={[
+          {
+            value: "jack",
+            label: "Jack",
+          },
+          {
+            value: "lucy",
+            label: "Lucy",
+          },
+          {
+            value: "tom",
+            label: "Tom",
+          },
+        ]}
+      />
+      <MyRadio.Group
+        name="radiogroup"
+        defaultValue={1}
+        options={[
+          { value: 1, label: "A" },
+          { value: 2, label: "B" },
+          { value: 3, label: "C" },
+          { value: 4, label: "D" },
+        ]}
+      />
+      <List
+        header={<div>Header</div>}
+        footer={<div>Footer</div>}
+        bordered
+        dataSource={data}
+        renderItem={(item) => <List.Item>{item}</List.Item>}
+      />
+      <MyTable dataSource={dataSource} columns={columns} />
+    </div>
   );
 };
 
